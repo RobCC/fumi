399ff574fe55cc20c184f580c246a60d26fa5110
RobCC <rrc0138@gmail.com>
2026-02-08 17:38:29 -0600

    feat: add im english fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/im-fell-english/400.css'
+import '@fontsource/im-fell-english/400-italic.css'
 import './index.css'
 import App from './App.tsx'
 
